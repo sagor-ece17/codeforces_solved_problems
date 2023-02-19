0b486f4536c4f5b484b642cb5b3d9633aa2ee4b0
sagor@55 <45065471+sagor-ece17@users.noreply.github.com>
2023-02-19 07:00:50 +0600

    Add files via upload

---
+++ cf 1472A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int w, h, n;
+        cin >> w >> h >> n;
+        int x, y, tot_cnt;
+        x = y = 0;
+        while(w > 0 && w % 2 == 0)
+        {
+            w /= 2;
+            x++;
+        }
+        while(h > 0 && h % 2 == 0)
+        {
+            h /= 2;
+            y++;
+        }
+        tot_cnt = 1 << (x + y);
+        if(tot_cnt >= n) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
+
